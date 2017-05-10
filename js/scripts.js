1c210a25f78bcc0ec715ba6bf9acd533c579892b
Daniela Trulls and Kristen McKee <daniela+kristen@>
2017-05-10 16:12:48 -0700

    add player 2 content in HTML and in javascript, add functionality in javascript to keep two separate scores for the two players.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,42 @@
 //business logic
-function Player(dice, arrayRollScore, rollScore, totalScore) {
+function Player(dice, rollScore, totalScore) {
   this.dice = dice;
-  this.arrayRollScore = arrayRollScore;
+  this.arrayRollScore = []; //START EMPTY
   this.rollScore = rollScore;
   this.totalScore = totalScore;
 }
+//WE NEED TO DECLARE THE ARRAY AS AN EMPTY ARRAY, BUT IT IS NOT PART OF OUR CONSTRUCTOR 'PARAMETERS'...SO WE DON'T OVERWRITE OUR ARRAY EVERY TIME A PLAYER ROLL THE DICE.
 
 // function Game(playerOne, playerTwo) {
 //   this.playerOne = playerOne;
 //   this.playerTwo = playerTwo;
 // }
-
-
-Player.prototype.Roll  = function(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(7);
-  this.dice = Math.floor(Math.random() * (max - min)) + min;
-  // this.dice = document.getElementById("dice-number")
-  // this.dice.innerHTML = Math.floor((Math.random() * 6) + 1);
+Player.prototype.Roll  = function() {
+  this.dice = Math.floor((Math.random() * 6) + 1);
   return this.dice;
 }
 
-// Push into a global Array
-// var rollScore = []; // try to make it local, how
 Player.prototype.Rollscore = function (){
   if (this.dice > 1){
     this.arrayRollScore.push(this.dice);
-  } else if (this.dice === 1){
-    this.arrayRollScore = 0 ;
-    //Try to reset .turn-total
-    }
-  return this.arrayRollScore;
+    console.log(this);
+    return this.arrayRollScore;
+  // } else if (this.dice === 1){
+    // this.arrayRollScore = 0 ;
+  //   //Try to reset .turn-total
+  }
 }
 
 Player.prototype.SumArray = function (){
-  // this.arrayRollScore.split(",");
-  // debugger
   this.rollScore=0;
   for(var i in this.arrayRollScore) {
     this.rollScore += parseInt(this.arrayRollScore[i]);
-    if(this.rollScore >= 100){
+    if(this.rollScore > 30){
       alert("You won!")
-      this.dice = "";
     }
   }
   return this.rollScore;
 }
-
-
 
 
 
@@ -56,22 +45,31 @@ $(document).ready(function() {
   var dice;
   var rollScore;
   var totalScore;
-  var arrayRollScore = [];
-  var player1 = new Player(dice, arrayRollScore, rollScore, totalScore);
+  var player1 = new Player(dice, rollScore, totalScore);
+  var player2 = new Player(dice, rollScore, totalScore);
 
   $("button#roll").click(function(){
     $("#dice-number").text(player1.Roll());
     $(".turn-total").text(player1.Rollscore());
-
   });
 
-  $("button#hold").click(function(){
+  $("button#hold").last().click(function(){
     $(".over-all-total").text(player1.SumArray());
     $(".turn-total").text("");
     //Try to reset .turn-total
+  });
 
-    $("button#restart").click(function(){
-       location.reload();
-    })
+  $("button#roll-2").click(function(){
+    $("#dice-number-2").text(player2.Roll());
+    $(".turn-total-2").text(player2.Rollscore());
+  });
+
+  $("button#hold-2").last().click(function(){
+    $(".over-all-total-2").text(player2.SumArray());
+    $(".turn-total-2").text("");
+  });
+
+  $("button#restart").click(function(){
+    location.reload();
   });
 });
